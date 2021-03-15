9f7046ea0bcc555dafe164afa96d2bb12556b3c7
Takeshi Kaneko <target.ark@gmail.com>
2021-03-16 06:07:54 +0900

    Use tui-rs for layout

--- src/app.rs
+++ src/app.rs
@@ -5,12 +5,15 @@ use crossterm::{
     cursor,
     event::{read, Event, KeyCode, KeyEvent, KeyModifiers},
     execute,
-    style::Print,
-    terminal,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use git2::{Blob, Commit, Delta, ObjectType, Oid, Repository};
+use git2::{
+    Blob, Commit, Delta, DescribeFormatOptions, DescribeOptions, ObjectType, Oid, Repository,
+};
+use itertools::Itertools;
 use std::env;
-use std::io::stdout;
+use std::io;
+use tui::{backend::CrosstermBackend, layout, widgets, Terminal};
 pub struct App;
 
 #[derive(Debug)]
@@ -148,21 +151,16 @@ impl App {
             })
         }));
 
-        let mut stdout = stdout();
+        enable_raw_mode()?;
+        execute!(io::stdout(), EnterAlternateScreen, cursor::Hide)?;
 
-        terminal::enable_raw_mode()?;
-        execute!(stdout, terminal::EnterAlternateScreen, cursor::Hide)?;
+        let stdout = io::stdout();
+        let backend = CrosstermBackend::new(stdout);
+        let mut terminal = Terminal::new(backend)?;
+        terminal.clear()?;
 
         loop {
-            execute!(
-                stdout,
-                terminal::Clear(terminal::ClearType::All),
-                cursor::MoveTo(0, 0),
-                Print(format!(
-                    "{}",
-                    history.current().get_commit(&repo).summary().unwrap_or("")
-                ))
-            )?;
+            display(&mut terminal, &history, &repo)?;
             match read()? {
                 Event::Key(event) => match event {
                     KeyEvent {
@@ -172,6 +170,10 @@ impl App {
                     | KeyEvent {
                         code: KeyCode::Char('d'),
                         modifiers: KeyModifiers::CONTROL,
+                    }
+                    | KeyEvent {
+                        code: KeyCode::Esc,
+                        modifiers: _,
                     } => break,
                     KeyEvent {
                         code: KeyCode::Up,
@@ -185,46 +187,80 @@ impl App {
                         code: KeyCode::Right,
                         modifiers: _,
                     } => {
-                        if let Some(current) = history.go_foreward() {
-                            execute!(
-                                stdout,
-                                terminal::Clear(terminal::ClearType::All),
-                                cursor::MoveTo(0, 0),
-                                Print(format!(
-                                    "{}",
-                                    current.get_commit(&repo).summary().unwrap_or("")
-                                ))
-                            )?;
+                        if let Some(_) = history.go_foreward() {
+                            display(&mut terminal, &history, &repo)?;
                         }
                     }
                     KeyEvent {
                         code: KeyCode::Left,
                         modifiers: _,
                     } => {
-                        if let Some(current) = history.go_backward() {
-                            execute!(
-                                stdout,
-                                terminal::Clear(terminal::ClearType::All),
-                                cursor::MoveTo(0, 0),
-                                Print(format!(
-                                    "{}",
-                                    current.get_commit(&repo).summary().unwrap_or("")
-                                ))
-                            )?;
+                        if let Some(_) = history.go_backward() {
+                            display(&mut terminal, &history, &repo)?;
                         }
                     }
                     _ => {
-                        // debug
-                        execute!(stdout, Print(format!("{:?}", event)))?;
+                        //
                     }
                 },
                 _ => {}
             }
         }
 
-        execute!(stdout, cursor::Show, terminal::LeaveAlternateScreen)?;
-        terminal::disable_raw_mode()?;
+        execute!(io::stdout(), cursor::Show, LeaveAlternateScreen)?;
+        disable_raw_mode()?;
 
         Ok(())
     }
+}
+
+fn display<W: io::Write>(
+    terminal: &mut Terminal<CrosstermBackend<W>>,
+    history: &History,
+    repo: &Repository,
+) -> Result<()> {
+    let commit = history.current().get_commit(&repo);
+    let commit_id = commit.as_object().short_id()?;
+    let names = repo
+        .references()?
+        .names()
+        .filter_map(|name| name.ok())
+        .filter(|name| {
+            repo.refname_to_id(name)
+                .ok()
+                .filter(|oid| *oid == commit.id())
+                .is_some()
+        })
+        .join(" ");
+
+    let mut title = format!(" Commit: {} ", commit_id.as_str().unwrap());
+    if !names.is_empty() {
+        title.push_str(&(names + " "));
+    }
+
+    terminal.draw(|frame| {
+        let chunks = layout::Layout::default()
+            .direction(layout::Direction::Vertical)
+            .constraints(
+                [
+                    layout::Constraint::Length(3 + 2),
+                    layout::Constraint::Min(0),
+                ]
+                .as_ref(),
+            )
+            .split(frame.size());
+        let block = widgets::Block::default()
+            .title(title)
+            .borders(widgets::Borders::ALL);
+        frame.render_widget(block, chunks[0]);
+        let block = widgets::Block::default()
+            .title(format!(
+                "{}",
+                history.current().get_commit(&repo).summary().unwrap_or("")
+            ))
+            .borders(widgets::Borders::ALL);
+        frame.render_widget(block, chunks[1]);
+    })?;
+
+    Ok(())
 }
