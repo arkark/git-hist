97ff18378aed8e1edff6c786170ce4ff6e9813bc
Takeshi Kaneko <target.ark@gmail.com>
2021-03-15 21:35:45 +0900

    Show commit summaries for a given file

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,16 @@
 use crate::args::Args;
+
 use anyhow::{anyhow, Context, Result};
+use crossterm::{
+    cursor,
+    event::{read, Event, KeyCode, KeyEvent, KeyModifiers},
+    execute,
+    style::Print,
+    terminal,
+};
 use git2::{Blob, Commit, Delta, ObjectType, Oid, Repository};
 use std::env;
+use std::io::stdout;
 pub struct App;
 
 #[derive(Debug)]
@@ -40,6 +49,42 @@ impl TurningPoint {
     }
 }
 
+struct History {
+    points: Vec<TurningPoint>,
+    current_index: usize,
+}
+
+impl History {
+    pub fn new<I: Iterator<Item = TurningPoint>>(points: I) -> Self {
+        Self {
+            points: points.collect(),
+            current_index: 0,
+        }
+    }
+
+    pub fn current(&self) -> &TurningPoint {
+        self.points.get(self.current_index).unwrap()
+    }
+
+    pub fn go_backward(&mut self) -> Option<&TurningPoint> {
+        if self.current_index + 1 < self.points.len() {
+            self.current_index += 1;
+            Some(self.current())
+        } else {
+            None
+        }
+    }
+
+    pub fn go_foreward(&mut self) -> Option<&TurningPoint> {
+        if self.current_index > 0 {
+            self.current_index -= 1;
+            Some(self.current())
+        } else {
+            None
+        }
+    }
+}
+
 impl App {
     pub fn run(args: Args) -> Result<()> {
         let repo = Repository::discover(env::current_dir()?)
@@ -54,7 +99,7 @@ impl App {
         let commits = revwalk
             .map(|oid| oid.and_then(|oid| repo.find_commit(oid)).unwrap())
             .collect::<Vec<_>>();
-        let first_file_oid = commits
+        let latest_file_oid = commits
             .first()
             .context("Failed to get any commit")?
             .tree()
@@ -73,45 +118,112 @@ impl App {
             })?
             .id();
 
-        let mut file_oid = first_file_oid;
-        let history = commits
-            .iter()
-            .filter_map(|commit| {
-                let old_tree = commit.parent(0).and_then(|p| p.tree()).ok();
-                let new_tree = commit.tree().ok();
-                assert!(new_tree.is_some());
+        let mut file_oid = latest_file_oid;
+        let mut history = History::new(commits.iter().filter_map(|commit| {
+            let old_tree = commit.parent(0).and_then(|p| p.tree()).ok();
+            let new_tree = commit.tree().ok();
+            assert!(new_tree.is_some());
 
-                let mut diff = repo
-                    .diff_tree_to_tree(old_tree.as_ref(), new_tree.as_ref(), None)
-                    .unwrap();
+            let mut diff = repo
+                .diff_tree_to_tree(old_tree.as_ref(), new_tree.as_ref(), None)
+                .unwrap();
 
-                // Detect file renames, copies, etc.
-                diff.find_similar(None).unwrap();
+            // detect file renames, copies, etc.
+            diff.find_similar(None).unwrap();
 
-                let delta = diff
-                    .deltas()
-                    .find(|delta| delta.new_file().id() == file_oid);
-                if let Some(delta) = delta.as_ref() {
-                    file_oid = delta.old_file().id();
-                }
+            let delta = diff
+                .deltas()
+                .find(|delta| delta.new_file().id() == file_oid);
+            if let Some(delta) = delta.as_ref() {
+                file_oid = delta.old_file().id();
+            }
 
-                delta.map(|delta| {
-                    TurningPoint::new(
-                        commit.id(),
-                        delta.old_file().id(),
-                        delta.new_file().id(),
-                        delta.status(),
-                    )
-                })
+            delta.map(|delta| {
+                TurningPoint::new(
+                    commit.id(),
+                    delta.old_file().id(),
+                    delta.new_file().id(),
+                    delta.status(),
+                )
             })
-            .collect::<Vec<_>>();
+        }));
 
-        for point in history {
-            println!("{:?}", point);
-            println!("{:?}", point.get_commit(&repo));
-            println!("{:?}", point.get_old_blob(&repo));
-            println!("{:?}", point.get_new_blob(&repo));
+        let mut stdout = stdout();
+
+        terminal::enable_raw_mode()?;
+        execute!(stdout, terminal::EnterAlternateScreen, cursor::Hide)?;
+
+        loop {
+            execute!(
+                stdout,
+                terminal::Clear(terminal::ClearType::All),
+                cursor::MoveTo(0, 0),
+                Print(format!(
+                    "{}",
+                    history.current().get_commit(&repo).summary().unwrap_or("")
+                ))
+            )?;
+            match read()? {
+                Event::Key(event) => match event {
+                    KeyEvent {
+                        code: KeyCode::Char('c'),
+                        modifiers: KeyModifiers::CONTROL,
+                    }
+                    | KeyEvent {
+                        code: KeyCode::Char('d'),
+                        modifiers: KeyModifiers::CONTROL,
+                    } => break,
+                    KeyEvent {
+                        code: KeyCode::Up,
+                        modifiers: _,
+                    } => {}
+                    KeyEvent {
+                        code: KeyCode::Down,
+                        modifiers: _,
+                    } => {}
+                    KeyEvent {
+                        code: KeyCode::Right,
+                        modifiers: _,
+                    } => {
+                        if let Some(current) = history.go_foreward() {
+                            execute!(
+                                stdout,
+                                terminal::Clear(terminal::ClearType::All),
+                                cursor::MoveTo(0, 0),
+                                Print(format!(
+                                    "{}",
+                                    current.get_commit(&repo).summary().unwrap_or("")
+                                ))
+                            )?;
+                        }
+                    }
+                    KeyEvent {
+                        code: KeyCode::Left,
+                        modifiers: _,
+                    } => {
+                        if let Some(current) = history.go_backward() {
+                            execute!(
+                                stdout,
+                                terminal::Clear(terminal::ClearType::All),
+                                cursor::MoveTo(0, 0),
+                                Print(format!(
+                                    "{}",
+                                    current.get_commit(&repo).summary().unwrap_or("")
+                                ))
+                            )?;
+                        }
+                    }
+                    _ => {
+                        // debug
+                        execute!(stdout, Print(format!("{:?}", event)))?;
+                    }
+                },
+                _ => {}
+            }
         }
+
+        execute!(stdout, cursor::Show, terminal::LeaveAlternateScreen)?;
+        terminal::disable_raw_mode()?;
 
         Ok(())
     }
